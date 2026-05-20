6702c0e0fff1139ab1e53b01a3b4c0f37520f1ca
Max Laubstein <152578824+maxlaubstein@users.noreply.github.com>
2026-05-20 18:59:18 -0400

    Update metadata file paths and filtering criteria

--- Thryomanes_Discrete/differential_expression.r
+++ Thryomanes_Discrete/differential_expression.r
@@ -5,9 +5,11 @@ library(dplyr)
 library(tximport)
 library(grid)
 library(tidyr)
-
+set.seed(16)
 setwd("/n/home00/mlaubstein/climatechangeRNA/Thryomanes_Discrete/")
 system("rm *sig_DEGs.csv")
+system("rm *all_genes.csv")
+system("rm combined_output_Thryomanes.csv")
 system("rm *Volcano.pdf")
 system("rm BEWR_Specific_PCA_All_Tissues.pdf")
 system("rm Summary_Plot.pdf")
@@ -16,11 +18,11 @@ system("rm Thryomanes_Figures.pdf")
 load("/n/holylfs06/LABS/edwards_lab/Lab/maxlaubstein/climatechangeRNA/Thryomanes_output/kallisto/deseq2_qc/deseq2.dds.RData")
 dds_og <- dds
 rm("dds")
-metadata <- read.csv('/n/holylfs06/LABS/edwards_lab/Lab/maxlaubstein/climatechangeRNA/Thryomanes/Nest_Box_RNA.csv', header = TRUE, sep = ",", check.names = FALSE)
+metadata <- read.csv('~/climatechangeRNA/Metadata_Climate_Change_RNA_Final.csv', header = TRUE, sep = ",", check.names = FALSE)
 
 metadata$id <- paste0(metadata$`Catalog Number`, "_", metadata$Tissue)
 metadata <- subset(metadata, metadata$Species == "Thryomanes bewickii")
-metadata <- metadata %>% select("id", "Tissue", "Treatment", "Sex", Individual = "Catalog Number")
+metadata <- metadata %>% dplyr::select("id", "Tissue", "Treatment", "Sex", Individual = "Catalog Number")
 
 rownames(metadata) <- metadata$id
 metadata <- metadata[colnames(dds_og), ]
@@ -33,7 +35,7 @@ colData(dds) <- DataFrame(metadata)
 dds$Tissue <- factor(dds$Tissue)
 dds$Treatment <- factor(dds$Treatment)
 design(dds) <- ~ Treatment + Tissue
-keep <- rowSums(counts(dds) > 0) >= 3 # only keep data points where the gene count is greater than 0 in at least 3 samples 
+keep <- rowSums(counts(dds) >= 10) >= 3 # only keep data points where the gene count is greater than 10 in at least 3 samples 
 dds <- dds[keep,]
 dds <- DESeq(dds)
 res <- results(dds, contrast = list("Treatment_Heat_vs_Control"), alpha = 0.05)
@@ -72,7 +74,7 @@ for(i in 1:length(tissues)){
   dds$Treatment <- factor(dds$Treatment)
   dds <- dds[, colData(dds)$Tissue == tissues[i]]
   design(dds) <- ~ Treatment
-  keep <- rowSums(counts(dds) > 0) >= 3 # only keep data points where the gene count is greater than 0 in at least 3 samples 
+  keep <- rowSums(counts(dds) >= 10) >= 3 # only keep data points where the gene count is greater than 10 in at least 3 samples 
   dds <- dds[keep,]
   dds <- DESeq(dds)
   res <- results(dds, contrast = list("Treatment_Heat_vs_Control"), alpha = 0.05)
@@ -106,24 +108,26 @@ for(i in 1:length(tissues)){
   assign(paste0(tissues[i], "_volcano_plot"), volcanoplot)
   message(paste0("Volcano plot stored in '", paste0(tissues[i], "_volcano_plot","'")))
   
-  DEG_df <- subset(volcano, abs(volcano$log2FoldChange) >= 1 & volcano$padj <= 0.05)
+  sig_DEG_df <- subset(volcano, abs(volcano$log2FoldChange) >= 1 & volcano$padj <= 0.05)
   
-  write.csv(DEG_df, file = paste0(tissues[i], "_sig_DEGs.csv"))
+  write.csv(sig_DEG_df, file = paste0(tissues[i], "_sig_DEGs.csv"))
   message(paste0("Significant DEGs file stored in '", paste0(tissues[i], "_sig_DEGs.csv","'")))
-  if(nrow(DEG_df) == 1){
+  if(nrow(sig_DEG_df) == 1){
     message(paste0("1 significant DEG was identified in ", tissues[i]))
   }
-  if(nrow(DEG_df) > 1){
-    message(paste0(nrow(DEG_df), " DEGs were identified in ", tissues[i]))
+  if(nrow(sig_DEG_df) > 1){
+    message(paste0(nrow(sig_DEG_df), " DEGs were identified in ", tissues[i]))
   }
-  if(nrow(DEG_df) == 0){
+  if(nrow(sig_DEG_df) == 0){
     message(paste0("No significant DEGs were identified in ", tissues[i]))
   }
   
+  all_genes_df <- volcano
+  all_genes_df$sig <- NULL
+  write.csv(all_genes_df, file = paste0(tissues[i], "_all_genes.csv"))
   
   message("\n")
 }
-
 
 all_plots <- list(
   all_PCA = all_PCA_plot,
@@ -140,7 +144,7 @@ all_plots <- list(
   kidney_volcano = kidney_volcano_plot,
   
   brain_PCA = brain_PCA_plot,
-  brain_volcano = brain_volcano_plot,
+  brain_volcano = brain_volcano_plot
 )
 
 pdf("Thryomanes_Figures.pdf", width = 8.5, height = 11)
@@ -211,7 +215,7 @@ summary_plot_df <- data.frame(
 )
 
 summary_plot_df_long <- summary_plot_df |>
-  select(Tissue, nUP, nDOWN) |>
+  dplyr::select(Tissue, nUP, nDOWN) |>
   pivot_longer(cols = c(nUP, nDOWN),
                names_to = "UPorDOWN",
                values_to = "Count")
@@ -226,4 +230,21 @@ summary_plot <- ggplot(summary_plot_df_long, aes(x = Tissue, y = Count, fill = U
 
 ggsave("Summary_Plot.pdf", summary_plot, width = 6, height = 4, units = "in")
 
+heart_genes <- read.csv("heart_all_genes.csv")
+heart_genes$tissue <- "heart"
+
+liver_genes <- read.csv("liver_all_genes.csv")
+liver_genes$tissue <- "liver"
+
+muscle_genes <- read.csv("muscle_all_genes.csv")
+muscle_genes$tissue <- "muscle"
+
+kidney_genes <- read.csv("kidney_all_genes.csv")
+kidney_genes$tissue <- "kidney"
+
+brain_genes <- read.csv("brain_all_genes.csv")
+brain_genes$tissue <- "brain"
+
+combined_output <- rbind(heart_genes, liver_genes, muscle_genes, kidney_genes, brain_genes)
+write.csv(combined_output, file = "combined_output_Thryomanes.csv")
 
